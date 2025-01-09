dafa83d5590fca226a62134c8c91634543108948
stewartrowley <stewy.rowley@gmail.com>
2025-01-08 19:10:12 -0800

    updating code

--- keyforge-go/src/views/CardView.vue
+++ keyforge-go/src/views/CardView.vue
@@ -1,6 +1,17 @@
 <template>
    <div>
-      <div>{{ allCardsGroup }}</div>
+      <table>
+         <tbody>
+            <tr v-for="card in AllCards">
+               <td>{{ card.card_title }}</td>
+               <td>{{ card.house }}</td>
+               <td>{{ card.card_type }}</td>
+               <td>{{ card.rarity }}</td>
+               <td>{{ this.cardQuantity(card._id) }}</td>
+               <!-- <td>{{ card }}</td> -->
+            </tr>
+         </tbody>
+      </table>
       <Gallery />
    </div>
 </template>
@@ -13,9 +24,22 @@ export default {
       Gallery
    },
    computed: {
+      AllCards () {
+         console.log(useCardStore().allCards)
+         return useCardStore().allCards;
+      },
       CardCount () {
          console.log(useCardStore().allCardsGroup);
          return useCardStore().allCardsGroup;
+      }
+   },
+   methods: {
+      cardQuantity (cardId) {
+         if (useCardStore().allCardsGroup[cardId]) {
+            return useCardStore().allCardsGroup[cardId].length;
+         } else {
+            return 0;
+         }
       }
    }
 }
